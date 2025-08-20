6ff933c41db0680da80726903f02694543f7224d
Peter Sambo <peter2great2@gmail.com>
2025-08-20 12:23:59 +0100

    chore: initialize package.json for shop-app with dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(name) {
-    return `Hello, ${name}!`;
-}
-console.log(greet("World"));
-//# sourceMappingURL=index.js.map
