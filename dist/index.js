cd42a8ce450f793686926c777fd8dc454c290a2c
Peter Sambo <peter2great2@gmail.com>
2025-08-20 13:11:19 +0100

    feat: initialize TypeScript setup with Express and MongoDB
    - Added TypeScript support with necessary dependencies in package.json
    - Created a basic Express server in src/index.ts
    - Integrated dotenv for environment variable management
    - Established MongoDB connection using mongoose
    - Defined User schema in src/schemas/UserSchema.ts with TypeScript interfaces
    - Added .env file for configuration of PORT and MONGO_URL

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,23 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const mongoose_1 = __importDefault(require("mongoose"));
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/shopify-app";
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    mongoose_1.default
+        .connect(mongoUrl)
+        .then(() => {
+        console.log("Connected to MongoDB");
+    })
+        .catch((error) => {
+        console.error("MongoDB connection error:", error);
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Application } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import UserSchema from "./schemas/UserSchema";
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+const mongoUrl =
+  (process.env.MONGO_URL as string) || "mongodb://127.0.0.1:27017/shopify-app";
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  mongoose
+    .connect(mongoUrl)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.error("MongoDB connection error:", error);
+    });
+});
